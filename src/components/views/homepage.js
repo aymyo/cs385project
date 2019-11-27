bf1334d3c152fe269952491891bcd8208962484e
aymyousfan <ayman.yousfan01@estudiant.upf.edu>
2019-11-27 15:31:24 +0000

    more setup things

---
+++ src/components/views/homepage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default class Homepage extends React.Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+                <div className="view home">
+                    <h1>APP TITLEadaldjalkdalkdjakld</h1>
+                </div>
+        );
+    }
+}
