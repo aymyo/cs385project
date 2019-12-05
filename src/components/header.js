8f89d46a04b88cef5ef9cf1f57d400376f7a68c1
aymyousfan <ayman.yousfan01@estudiant.upf.edu>
2019-12-05 11:19:58 +0000

    Header polished

--- src/components/header.js
+++ src/components/header.js
@@ -1,58 +1,43 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 
 export default class Header extends React.Component {
 
     render() {
         return (
-            <div>
-            <header className="provisional-header">Header
-                <span className="header-link">
-                    <NavLink to='/'>Home</NavLink>
-                </span>
-                <span className="header-link">
-                    <NavLink to='/recipes'>My recipes</NavLink>
-                </span>
-            </header>
             <header className="header">
-                <nav className="navbar navbar-light bg-light">
-                    <div className="col-sm-3 text-center" id="dropdown">
-                        <div className="pos-f-t">
-                            <button className="navbar-toggler" type="button" data-toggle="collapse"
-                                    data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
-                                    aria-expanded="false" aria-label="Toggle navigation">
-                                <span className="navbar-toggler-icon"></span>
-                            </button>
-                            <div className="collapse" id="navbarToggleExternalContent">
-                                <div className="bg-light p-4 text-black">
-                                    <ul className="nav navbar-nav">
-                                        <li className="active">
-                                            <span className="header-link">
-                                                <NavLink to='/'>Home</NavLink>
-                                            </span></li>
-                                        <li>Add a recipe</li>
-                                        <li>
-                                            <span className="header-link">
-                                                <NavLink to='/recipes'>My recipes</NavLink>
-                                            </span>
-                                        </li>
-                                        <li>Help</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-sm-6 text-center" id="appName">
-                        <h3><b>Nutri</b>meal</h3>
-                    </div>
-                    <div className="col-sm-3 text-center">
-                        <span className="glyphicon glyphicon-user">   Login(?)</span>
-                    </div>
+                <div className="header-view">
+                    <button className="navbar-toggler h-toggle" type="button" data-toggle="collapse"
+                            data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon my-toggler"></span>
+                    </button>
+                    <span className="h-title"><b>Nutri</b>meal</span>
+                    <FontAwesomeIcon icon={faUser} className="h-user" ></FontAwesomeIcon>
+                </div>
 
-                </nav>
+                <div className="col-sm-3 text-center header-navbar" id="dropdown">
+                    <div className="collapse" id="navbarToggleExternalContent">
+                        <nav className="nav navbar-nav h-links">
+                            <span className="header-link">
+                                <NavLink to='/'>Home</NavLink>
+                            </span>
+                            <span className="header-link">
+                                <NavLink to='/create'>Create recipe</NavLink>
+                            </span>
+                            <span className="header-link">
+                                <NavLink to='/recipes'>My recipes</NavLink>
+                            </span>
+                            <span className="header-link">
+                                <NavLink to='/help'>Help</NavLink>
+                            </span>
+                        </nav>
+                    </div>
+                </div>
 
             </header>
-            </div>
         );
     }
 }
