4f371c549cb17cdfbf4b58aedb16d4465741865f
anajimmat <a.jmatilla@alumnos.upm.es>
2019-12-13 13:45:25 +0000

    image wrong

--- src/components/views/errorView.js
+++ src/components/views/errorView.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import image from '../../images/brokenphone.png';
 
 export default class errorView extends React.Component {
     state = {
@@ -13,6 +14,8 @@ export default class errorView extends React.Component {
                 <h5>Uh Oh...! Sorry, the page you were looking for doesn't exist.</h5>
                 <br/><p>You might have typed in the wrong address or the page has moved. In the meantime, try again or
                     return to the homepage :)</p>
+                <br/>
+                <img className="image" src={image}></img>
 
                 <div className="menuError">
                     <span className="header-link">
